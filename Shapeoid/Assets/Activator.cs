e132c0bb57e1edc1331b611db66773cfeb101f83
Phobez <abiaph@live.com>
2019-02-26 11:00:09 +0700

    Added Activator and Note
    - Created activator base code (no different shapes yet)
    - Created note base code (no different shapes yet)

---
+++ Shapeoid/Assets/Activator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    public KeyCode fireKey;
+    public KeyCode airKey;
+    public KeyCode waterKey;
+    public KeyCode earthKey;
+
+    private GameObject note;
+
+    private bool active;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        active = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (active)
+        {
+            if (Input.GetKeyDown(fireKey))
+            {
+                Destroy(note);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Note"))
+        {
+            active = true;
+            note = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        active = false;
+    }
+}
