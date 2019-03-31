a8562e3c4b14dd856656fe4d8db6b97cbffe7808
Phobez <abiaph@live.com>
2019-03-31 17:40:04 +0700

    Created prototype Main Menu, Song Selection, and Credits

---
+++ Shapeoid/Assets/Scripts/UI/QuitOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Made by: Abia P.H.
+// Description: Holds a method to quit the game to be used by buttons.
+public class QuitOnClick : MonoBehaviour
+{
+    // exits play mode if in editor, exits the game if not
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
---
+++ Shapeoid/Assets/Scripts/UI/SelectOnInput.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+// Made by: Abia P.H.
+// Description: selects selectedObject on any given input from the Vertical
+//              axis allowing the player to select buttons using the arrow keys
+public class SelectOnInput : MonoBehaviour
+{
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool buttonIsSelected;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") != 0 && !buttonIsSelected)
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonIsSelected = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        buttonIsSelected = false;
+    }
+}
