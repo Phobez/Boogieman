9d5ad67b0ef83f724186bd0e3c402e9965160ae3
Phobez <abiaph@live.com>
2019-03-31 20:30:56 +0700

    Added song selection scrolling

---
+++ Shapeoid/Assets/Scripts/UI/ScrollContent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+// Made by: Abia P.H.
+// Description: handles scroll content behaviour
+public class ScrollContent : MonoBehaviour, ISelectHandler
+{
+    private ScrollRectController scrollRectController;
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        scrollRectController = GetComponentInParent<ScrollRectController>();
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        Debug.Log(gameObject.name);
+        scrollRectController.SnapToSelected(rectTransform);
+    }
+}
---
+++ Shapeoid/Assets/Scripts/UI/Test/RectTransformMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RectTransformMove : MonoBehaviour
+{
+    public RectTransform a;
+    public ScrollRect b;
+    public RectTransform c;
+    public RectTransform d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            a.anchoredPosition = Vector3.zero;
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            b.verticalNormalizedPosition = 0.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            b.content.anchoredPosition = tempMethod();
+        }
+    }
+
+    public Vector2 tempMethod()
+    {
+        Canvas.ForceUpdateCanvases();
+
+        Vector2 viewportLocalPosition = d.localPosition;
+        Vector2 childLocalPosition = gameObject.GetComponent<RectTransform>().localPosition;
+        Vector2 result = new Vector2(/*0 - (viewportLocalPosition.x + childLocalPosition.x)*/ b.content.localPosition.x, 0 - (viewportLocalPosition.y + childLocalPosition.y));
+        return result;
+    }
+}
