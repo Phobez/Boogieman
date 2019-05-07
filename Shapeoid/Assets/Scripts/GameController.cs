82b773cc9f13aceb471c0f879c1d9bf869cfe240
Phobez <abiaph@live.com>
2019-05-07 11:10:05 +0700

    Implemented losing on energy hitting 0.

--- Shapeoid/Assets/Scripts/GameController.cs
+++ Shapeoid/Assets/Scripts/GameController.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// A component handling pausing.
+/// A component handling pausing and losing.
 /// </summary>
 public class GameController : MonoBehaviour
 {
     public SongController songController;
     public ActivatorController activatorController;
     public NoteGenerator noteGenerator;
+    public ScoreHandler scoreHandler;
     public GameObject pausePanel;
+    public GameObject losePanel;
 
     // Update is called once per frame
     private void Update()
     {
+        if (scoreHandler.energy <= 0)
+        {
+            Lose();
+        }
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Pause();
@@ -42,5 +49,17 @@ public class GameController : MonoBehaviour
         Time.timeScale = 1.0f;
         activatorController.PauseActivator(false);
         noteGenerator.PauseNotes(false);
+    }
+
+    /// <summary>
+    /// Ends the game after energy hits 0.
+    /// </summary>
+    public void Lose()
+    {
+        songController.PauseSong(true);
+        Time.timeScale = 0.0f;
+        activatorController.PauseActivator(true);
+        noteGenerator.PauseNotes(true);
+        losePanel.SetActive(true);
     }
 }
