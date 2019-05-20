c8e140f97fe10a9ebf3949f99de30456e9fb1016
Phobez <abiaph@live.com>
2019-05-21 01:11:34 +0700

    Fixed backgorund music bug.

--- Shapeoid/Assets/Scripts/BackgroundMusicPlayer.cs
+++ Shapeoid/Assets/Scripts/BackgroundMusicPlayer.cs
@@ -5,27 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class BackgroundMusicPlayer : MonoBehaviour
 {
+    private static BackgroundMusicPlayer instance = null;
+
     private AudioSource audioSource;
     private bool isPaused;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+            return;
+        }
+        Destroy(this.gameObject);
+    }
 
     // Start is called before the first frame update
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
 
-        DontDestroyOnLoad(gameObject);
         isPaused = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (SceneManager.GetActiveScene().buildIndex == 2 && !isPaused)
+        if ((SceneManager.GetActiveScene().buildIndex == 2 || SceneManager.GetActiveScene().buildIndex == 1) && !isPaused)
         {
             audioSource.Pause();
             isPaused = true;
         }
-        else if (SceneManager.GetActiveScene().buildIndex != 2 && isPaused)
+        else if ((SceneManager.GetActiveScene().buildIndex != 2 && SceneManager.GetActiveScene().buildIndex != 1) && isPaused)
         {
             audioSource.Play();
             isPaused = false;
